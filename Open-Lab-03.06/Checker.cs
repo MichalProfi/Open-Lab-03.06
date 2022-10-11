41a0f753154172062d0f8ff3732a82db797c127e
Michal Holena <113421705+MichalProfi@users.noreply.github.com>
2022-10-11 09:45:37 +0200

    check if string has space

--- Open-Lab-03.06/Checker.cs
+++ Open-Lab-03.06/Checker.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_03._06
     {
         public bool HasSpaces(string str)
         {
-            throw new NotImplementedException();
+            if (str.IndexOf(" ") >= 0)
+                return true;
+            else
+                return false;
         }
     }
 }
